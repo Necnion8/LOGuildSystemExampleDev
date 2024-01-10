47a958547b6f7ea18fcb606d653d85e2d5cad7e4
Necnion8 <necnionch@gmail.com>
2024-01-10 20:39:37 +0900

    first commit

---
+++ src/main/java/com/gmail/necnionch/myplugin/guildsystemexampledev/bukkit/GuildSystemExampleDevPlugin.java
@@ -0,0 +1,17 @@
+package com.gmail.necnionch.myplugin.guildsystemexampledev.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GuildSystemExampleDevPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
